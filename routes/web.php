3b92b5cbce3e13cabd2e769430713b61971e89f5
kanno-y <kanno0521@gmail.com>
2023-06-28 09:45:04 +0900

    initial

---
+++ routes/web.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Http\Controllers\UtilityController;
+use App\Http\Controllers\GameController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/hello_world', fn () =>  view('hello_world'));
+
+Route::get('/hello', fn () =>  view('hello', ["name" => "山田", "course" => "Laravel9"]));
+
+Route::get('/', fn () => view('index'));
+
+Route::get('/curriculum', fn () => view('curriculum'));
+
+// 世界の時間
+Route::get('/world-time', [UtilityController::class, 'worldTime']);
+
+// おみくじ
+Route::get('/omikuji', [GameController::class, 'omikuji']);
+
+// モンティ・ホール問題
+Route::get('/monty-hall',[GameController::class, 'montyHall']);
