97d3b80ed802831ec283e463a7aa3d7e33bfac2f
kanno-y <kanno0521@gmail.com>
2023-07-11 07:57:38 +0900

    🔨 Add: loginForm

--- app/Http/Controllers/RequestSampleController.php
+++ app/Http/Controllers/RequestSampleController.php
@@ -29,5 +29,17 @@ class RequestSampleController extends Controller
         return 'プロフィールページのURLは'.$url;
 
     }
-    
+
+    // ダミーログイン
+    public function login(Request $request) {
+        if ($request->get('email') === 'user@example.com' && $request->get('password') === '12345678') {
+            return 'ログイン成功';
+        }
+        return 'ログイン失敗';
+    }
+
+    // ログインページ
+    public function loginForm() {
+        return view('login');
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -43,3 +43,7 @@ Route::get('/query-strings', [RequestSampleController::class, 'queryStrings']);
 Route::get('/users/{id}', [RequestSampleController::class, 'profile'])->name(name: 'profile');
 Route::get('/products/{category}/{year}', [RequestSampleController::class, 'productsArchive']);
 Route::get('/route-link', [RequestSampleController::class, 'routeLink']);
+
+// ログイン
+Route::get('/login', [RequestSampleController::class, 'loginForm']);
+Route::post('/login', [RequestSampleController::class, 'login'])->name(name: 'login');
